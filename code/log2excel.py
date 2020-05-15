18fb0476b492e4d31502ddc01e4d1c9d7fa384c3
JoseJimmy <jose.jimmy@gmail.com>
2020-05-15 21:47:22 +0100

    LOG-12 #Done #Results tab

--- code/log2excel.py
+++ code/log2excel.py
@@ -25,15 +25,17 @@ def Log2Dataframe(LogFilename):
             date = line.split(' ')[-2]
 
         if('testname =' in line):
-            AimName=line.split('=')[1].split('\n')[0].replace(' ', '_')
+            AimName=line.split('=')[1].split('\n')[0]
 
         if ('---- Step:' in line):
-            temp = line.split('----')[1].strip()
+            temp = line.split('----')[1].strip().split()
+            temp = (temp[1]) if len(temp) == 2 else StepNo
             timestamp = line.split(' ')[0]
 
             if((LogAppended == False) and (StepNo !='NotFound')): # to add steps which does not have any variables to check / comments
                 FailsInAim.append([date + ' ' + timestamp, AimName, MacroName, StepNo, MacroStepNo, StepType, '--', '--'])
             LogAppended = False
+
             if (StepType == 'Main'):
                 StepNo = temp
             else:
@@ -77,8 +79,65 @@ LogFilename = filedialog.askopenfilename(title = "Select log file from PTAF ",fi
 csvFilename = LogFilename.replace('.txt','_report.csv')
 
 
+
 AimData = Log2Dataframe(LogFilename)
-AimData.to_csv(csvFilename,index = False)
+AimData.to_csv("v.csv",index = False)
 
+#%%
+#Aim	Step	REQ	Test Outcome	Test Date	Tester Name	Tester Comments	Systems/Software Analysis Date	Engineer Name	Engineer Comments	Test Result
+out = pd.DataFrame() #creates a new dataframe that's empty
+cols =  ['Aim Name','Step','REQ','Test Outcome','Date','Tester Name','Tester Comment']
+#%%
+steps = []
+outcome = []
+comment = []
+aimid = []
+no = []
+blanks=[]
+dates = []
+out = pd.DataFrame( columns=cols)
+for i,aim in enumerate(pd.unique(AimData.AimName)):
+    df = AimData[AimData.AimName == aim]
+    for step in pd.unique(df.StepNo):
 
+        temp = df[df.StepNo == step]
+        temp = temp.sort_values(['StepNo'])
+        result = '-'
+        text = ""
 
+        if ((temp['Outcome'] == 'Pass').any()):
+            result = 'Pass'
+            text = ""
+
+        if((temp['Outcome']=='Fail').any()):
+            result = 'Fail'
+            text = ('\n'.join((temp[temp['Outcome']=='Fail'].comment.values)))
+
+        dates.append(max(temp['Datetime']).strftime("%d/%m/%y"))
+        blanks.append("")
+        aimid.append(aim)
+        outcome.append(result)
+        comment.append(text)
+        steps.append(step)
+        no.append(i+1)
+    summary = pd.DataFrame(list(zip(aimid,steps,blanks,outcome,dates,blanks,comment)), columns=cols)
+    out = pd.concat([out, summary], join="inner")
+
+out.to_csv("u.csv",index=False)
+#
+# #%% Write the dataframe without the header and index.
+# import xlsxwriter
+# tkinter.Tk().withdraw()
+# reportFilename = filedialog.askopenfilename(title = "Select 325 File ",filetypes = (("Excel files","*.xlsx"),("all files","*.*")))
+#%%
+#
+# with pd.ExcelWriter(reportFilename, engine="openpyxl", mode="a") as writer:
+#     df.to_excel(writer, sheet_name="Test Results", startrow=9,index = False,header= False)
+# wb = UpdateWorkbook(r'C:\Users\vince\Project\Spreadsheet.xlsx', worksheet=1)
+# df_2 = fd_frames['key_2']
+# wb['M6:M25'] = df_2['2017'].values
+# wb.save()
+# #%%
+#
+# with pd.ExcelWriter(reportFilename, engine='openpyxl', mode="a") as ew:
+#     out.to_excel(ew, startrow=9, startcol=0)
